051351dea44756b00a0b08096e049b1767adfa16
SI Xiaolong <sixiaolong2018@outlook.com>
2024-03-20 22:06:18 +0800

    前端与数据库对接，实现基本数据交互。

--- SeeMyServer/Pages/Dialogs/AddServer.xaml.cs
+++ SeeMyServer/Pages/Dialogs/AddServer.xaml.cs
@@ -19,10 +19,39 @@ namespace SeeMyServer.Pages.Dialogs
 {
     public sealed partial class AddServer : ContentDialog
     {
-        public CMSModel NSData { get; private set; }
+        public CMSModel CMSData { get; private set; }
         public AddServer(CMSModel cmsModel)
         {
             this.InitializeComponent();
+
+            // 将Dialog两个按钮点击事件绑定
+            PrimaryButtonClick += MyDialog_PrimaryButtonClick;
+            SecondaryButtonClick += MyDialog_SecondaryButtonClick;
+
+            // 初始化Dialog中的字段，使用传入的CMSModel对象的属性
+            CMSData = cmsModel;
+            DisplayNameTextBox.Text = cmsModel.Name;
+            HostIPTextBox.Text = cmsModel.HostIP;
+            HostPortTextBox.Text = cmsModel.HostPort;
+            SSHUserTextBox.Text = cmsModel.SSHUser;
+            SSHKeyTextBox.Text = cmsModel.SSHKey;
+            OSTypeTextBox.Text = cmsModel.OSType;
+        }
+
+        private void MyDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            // 在"确定"按钮点击事件中保存用户输入的内容
+            CMSData.Name = string.IsNullOrEmpty(DisplayNameTextBox.Text) ? "<unnamed>" : DisplayNameTextBox.Text;
+            CMSData.HostIP = HostIPTextBox.Text;
+            CMSData.HostPort = HostPortTextBox.Text;
+            CMSData.SSHUser = SSHUserTextBox.Text;
+            CMSData.SSHKey = SSHKeyTextBox.Text;
+            CMSData.OSType = OSTypeTextBox.Text;
+        }
+
+        private void MyDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            // 在"取消"按钮点击事件中不做任何操作
         }
     }
 }
--- SeeMyServer/Pages/HomePage.xaml.cs
+++ SeeMyServer/Pages/HomePage.xaml.cs
@@ -13,20 +13,71 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
 using Windows.ApplicationModel.Resources;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Microsoft.UI.Dispatching;
 
 namespace SeeMyServer.Pages
 {
     public sealed partial class HomePage : Page
     {
         ResourceLoader resourceLoader = new ResourceLoader();
+        private DispatcherQueue _dispatcherQueue;
 
         public HomePage()
         {
             this.InitializeComponent();
+
+            // 获取UI线程的DispatcherQueue
+            _dispatcherQueue = DispatcherQueue.GetForCurrentThread();
+
+            // 页面出事后化，加载数据
+            LoadData();
         }
+
+        private void LoadData()
+        {
+            // 实例化SQLiteHelper
+            SQLiteHelper dbHelper = new SQLiteHelper();
+
+            // 查询数据
+            List<CMSModel> dataList = dbHelper.QueryData();
+
+            // 将数据列表绑定到ListView
+            dataListView.ItemsSource = dataList;
+        }
+
+        //private void LoadData()
+        //{
+        //    // 在子线程中执行任务
+        //    Thread subThread = new Thread(new ThreadStart(() =>
+        //    {
+        //        while (true)
+        //        {
+        //            // 在UI线程上更新
+        //            _dispatcherQueue.TryEnqueue(() =>
+        //            {
+
+        //                // 实例化SQLiteHelper
+        //                SQLiteHelper dbHelper = new SQLiteHelper();
+
+        //                // 查询数据
+        //                List<CMSModel> dataList = dbHelper.QueryData();
+
+        //                // 将数据列表绑定到ListView
+        //                dataListView.ItemsSource = dataList;
+
+        //            });
+
+        //            // 延迟1s
+        //            Thread.Sleep(1000);
+        //        }
+        //    }));
+        //    subThread.Start();
+        //}
+
         // 添加/修改配置按钮点击
         private async void AddConfigButton_Click(object sender, RoutedEventArgs e)
         {
@@ -49,12 +100,12 @@ namespace SeeMyServer.Pages
             // 如果按下了Primary
             if (result == ContentDialogResult.Primary)
             {
-                //// 实例化SQLiteHelper
-                //SQLiteHelper dbHelper = new SQLiteHelper();
-                //// 插入新数据
-                //dbHelper.InsertData(initialCMSModelData);
-                //// 重新加载数据
-                //LoadData();
+                // 实例化SQLiteHelper
+                SQLiteHelper dbHelper = new SQLiteHelper();
+                // 插入新数据
+                dbHelper.InsertData(initialCMSModelData);
+                // 加载数据
+                LoadData();
             }
         }
     }
